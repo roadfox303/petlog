672536f02629d3c17c4ffa1787b0bf68f9ff1a1a
takamori nambu <roadfox303@gmail.com>
2021-02-24 16:59:36 +0900

    userモデルのバリデーションを調整(passwordなしでも更新可能に)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
